79d0c998dfcf2940e5dd4ba8e638fdb31cdec514
OlivierMartinez504 <57641304+OlivierMartinez504@users.noreply.github.com>
2020-04-18 16:28:26 +0200

    Add files via upload

---
+++ Km_to_Miles.py
@@ -0,0 +1,6 @@
+print("This program converts Kilometers to Miles")
+
+km = float( input("Please enter the distance in Km to converte in to Miles: ") )
+miles = km / 1.609344
+
+print(km, "km=", round(miles,4), "miles")
